9bc0c9f1c0266d7ca3a1e7acc211145b74cb52e5
klas9802 <klas9806@gmail.com>
2021-06-15 15:45:57 +0800

    ok

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,21 +1,24 @@
 import React, { Component, useEffect, useState } from 'react';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonBackButton, IonButtons, IonButton } from '@ionic/react'
 import { RouteComponentProps } from 'react-router';
-import { Link } from 'react-router-dom';
+
 import axios from 'axios';
 
 interface Reservoir{
     StationNo : any;
     Inflow:any;
     Outflow:any;
+    Time:any
 }
 
 
 export default function Details(props: RouteComponentProps<{id: string}>): JSX.Element{
     const {match} = props;
+    const {id} = match.params;
     const [reservoirs, setReservoirList] = useState<Reservoir[]>([]);
     
-   
+
+
     useEffect(() => {
         axios.get<Reservoir[]>('https://fhy.wra.gov.tw/WraApi/v1/Reservoir/RealTimeInfo')
         .then(response => {
@@ -23,17 +26,20 @@ export default function Details(props: RouteComponentProps<{id: string}>): JSX.E
             setReservoirList( response.data );
         })
     })
+
+    
     
     return(
+        
         <div>
-            <h1>{match.params.id}</h1>
-            <ul>
-            {reservoirs.map(water => (
-                <li key={match.params.id}>
-                    <p>{water.StationNo}</p>
-                </li>
-            ))}
-            </ul>
+            <h1>Url params: {id}</h1>
+            <div>
+                {
+                    reservoirs.filter(water => water.StationNo == "10201").map(water => (
+                        <li>{water.Time}</li>
+                    ))
+                }
+            </div>
         </div>
     )
 }
